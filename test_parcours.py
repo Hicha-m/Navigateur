5b3d8285f07eff377edd4bff8b4054dedc95335c
Hicha-m <68863445+Hicha-m@users.noreply.github.com>
2023-10-26 11:21:30 +0200

    Create test_parcours.py

---
+++ test_parcours.py
@@ -0,0 +1,16 @@
+""""ce programme contient les tests des fonctions contenus dans le fichier "parcours.py""" ""
+
+from parcours import parcours_en_largeur,test_parcours_en_largeur,trouver_chemin,test_trouver_chemin
+
+
+def test_parcours_en_largeur():
+  pass
+
+def test_test_parcours_en_largeur():
+  pass
+  
+def test_trouver_chemin():
+  pass
+
+def test_test_trouver_chemin():
+  pass
