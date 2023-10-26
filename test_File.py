97ab308e98bfcc1292ef35d5e1978b72a9d422d5
Hicha-m <68863445+Hicha-m@users.noreply.github.com>
2023-10-26 11:18:12 +0200

    Create test_File.py

---
+++ test_File.py
@@ -0,0 +1,18 @@
+""""ce programme contient les tests des méthodes contenus dans le fichier "File.py""" ""
+
+from File import File
+
+def test_nouvelle_file():
+  pass
+  
+def test_enfiler():
+  pass
+
+def test_defiler():
+  pass
+  
+def test_file_vide():
+  pass
+
+def test_test_file():
+  pass
