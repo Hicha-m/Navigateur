2495b3018e76416e2ad22341c3cd00900c2e9eaa
Acer13016 <147695862+Acer13016@users.noreply.github.com>
2023-10-26 11:36:19 +0200

    Update Graphe.py

--- Graphe.py
+++ Graphe.py
@@ -1,22 +1,44 @@
-def Graphe():
-  def __init__(self,n):
-    self.n = n
-    self.tableaux = []
-
-  def nouveau_graphe(self,n):
-    pass
-
-  def taille_graphe(self, G):
-    pass
-
-  def ajouter_arc(self, G, i, j):
-    pass
-
-  def existe_arc(self, G, i, j):
-    pass
-
-  def afficher_graphe(self, G):
-    pass
-
-  def test_graphe(self):
-    pass
+class Graphe:
+    def __init__(self,n):
+    
+        self.n = n
+        self.tableaux = []
+      
+        
+    def nouveau_graphe(self,n:int) -> list[list[int]]:
+        """methode qui renvoie un graphe de n sommets sans aucun arc"""
+        for i in n:
+            l = []
+            for j in n:
+                l.append(0)
+            self.tableaux.append(l)
+        return self.tableaux
+    
+    
+    def taille_graphe(self) -> int :
+        """methode qui renvoie le nombre de sommets du graphe G"""
+        return len(self.tableaux)
+    
+          
+    def ajouter_arc(self, i:int, j:int):
+        """methode qui ajoute un arc i → j au graphe G"""
+        for i in self.tableaux:
+            for j in range(i):
+                
+                
+            
+    
+      
+    def existe_arc(self, i:int, j:int) -> bool:
+        """methode qui renvoie True s’il y a un arc i → j dans le graphe G"""
+        pass
+    
+    
+    def afficher_graphe(self):
+        """methode qui affiche la matrice d’adjacence du graphe G sur l’écran"""
+        pass
+    
+    
+    def test_graphe(self):
+        """methode de test qui crée un graphe aléatoire et l’affiche sur l’écran"""
+        pass
