123547b21a6ec3f7f262cc475ca0053e7a95fe1b
Hicha-m <68863445+Hicha-m@users.noreply.github.com>
2023-10-26 11:16:32 +0200

    Create test_Graphe.py

---
+++ test_Graphe.py
@@ -0,0 +1,21 @@
+""""ce programme contient les tests des méthodes contenus dans le fichier "Graphe.py""" ""
+
+from Graphe import Graphe
+
+def test_nouveau_graphe():
+  pass
+
+def test_taille_graphe():
+  pass
+
+def test_ajouter_arc():
+  pass
+
+def test_existe_arc():
+  pass
+
+def test_afficher_graphe():
+  pass
+  
+def test_test_graphe():
+  pass
