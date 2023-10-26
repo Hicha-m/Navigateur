a8997c8a0e9e6a97534bb2f75926281b8d767d4a
Hicha-m <68863445+Hicha-m@users.noreply.github.com>
2023-10-26 10:28:29 +0200

    Create Graphe.py

---
+++ Graphe.py
@@ -0,0 +1,22 @@
+def Graphe():
+  def __init__(self,n):
+    self.n = n
+    self.tableaux = []
+
+  def nouveau_graphe(self,n):
+    pass
+
+  def taille_graphe(self, G):
+    pass
+
+  def ajouter_arc(self, G, i, j):
+    pass
+
+  def existe_arc(self, G, i, j):
+    pass
+
+  def afficher_graphe(self, G):
+    pass
+
+  def test_graphe(self):
+    pass
