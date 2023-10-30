e6b16eba1fbf9d6392437c6fe09a1a3c27ddbbca
Hicha-m <med.hicham@hotmail.fr>
2023-10-30 16:26:33 +0100

    testes Graphe

--- test_Graphe.py
+++ test_Graphe.py
@@ -3,25 +3,44 @@
 from Graphe import Graphe
 
 
+def test_instance():
+    taille = 3
+    g = Graphe(taille)
+
+    assert isinstance(g, Graphe) == True
+
+
 def test_nouveau_graphe():
-    pass
+    taille = 3
+    g = Graphe(taille)
+    g.nouveau_graphe()
+    assert g.tableaux == [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
 
 def test_taille_graphe():
-    pass
+    taille = 3
+    g = Graphe(taille)
+    g.nouveau_graphe()
+    assert g.taille_graphe() == taille
+    assert g.n == taille
 
 
 def test_ajouter_arc():
-    pass
+    taille = 3
+    g = Graphe(taille)
+    g.nouveau_graphe()
+    g.ajouter_arc(0, 0)
+    assert g.tableaux == [[1, 0, 0], [0, 0, 0], [0, 0, 0]]
+    g.ajouter_arc(1, 1)
+    assert g.tableaux == [[1, 0, 0], [0, 1, 0], [0, 0, 0]]
+    g.ajouter_arc(2, 2)
+    assert g.tableaux == [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
 
 
 def test_existe_arc():
-    pass
-
-
-def test_afficher_graphe():
-    pass
-
-
-def test_test_graphe():
-    pass
+    taille = 3
+    g = Graphe(taille)
+    g.nouveau_graphe()
+    assert g.existe_arc(2,2) == False
+    g.ajouter_arc(0, 0)
+    assert g.existe_arc(0,0) == True
