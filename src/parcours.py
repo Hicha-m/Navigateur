b7c937f9de954e0b87ab4a581eca3c8dd7eac1d4
Acer13016 <147695862+Acer13016@users.noreply.github.com>
2023-11-09 11:32:18 +0100

    parcours.py

--- src/parcours.py
+++ src/parcours.py
@@ -1,9 +1,27 @@
-from src.Graphe import Graphe
-from src.File import File
-
+from Graphe import Graphe
+from File import File
 
 def parcours_en_largeur(G, s):
-    pass
+    """Cette fonction renvoie un graphe H qui contient les sommets de G 
+    et seulement les arcs parcourus en explorant le graphe à partir du sommet s."""
+    n = len(G)
+    couleur = [-1]*n
+    for i in range(n-1):
+        couleur[i] = "blanc"
+    H = Graphe(n)
+    F = File()
+    couleur[s] = "rouge"
+    F.enfiler(s)
+    while len(F) != 0:
+        i = F.defiler()
+        for j in range(n-1):
+            if G.existe_arc(i,j) == True and couleur[j] == "blanc":
+                couleur[j] = "rouge"
+                H.ajouter_arc(i,j)
+                F.enfiler(j)
+        couleur[i] = "vert"
+    return H
+    
 
 
 def test_parcours_en_largeur():
